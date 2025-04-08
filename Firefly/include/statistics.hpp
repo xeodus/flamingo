53927a95685de7418b99a2a55b34413e20301133
Kaustab Bharasa <accessphysics537@gmail.com>
2025-04-08 13:05:43 +0530

    Initial commit

---
+++ Firefly/include/statistics.hpp
@@ -0,0 +1,10 @@
+#pragma once 
+
+class Statistics {
+
+public:
+    static double normal_CDF(double x);
+    static double normal_PDF(double x);
+    static double normal_CDF_Inverse(double p);
+    
+};
